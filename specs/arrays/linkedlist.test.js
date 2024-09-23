bef8a13655c71f36b391254e70037ac231ab8e74
dinosawrfish <dinosawrfish@gmail.com>
2024-09-22 17:09:32 -0700

    linked list solution

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -1,21 +1,21 @@
 /*
   LinkedList
-  
+
   Name your class / constructor (something you can call new on) LinkedList
-  
+
   LinkedList is made by making nodes that have two properties, the value that's being stored and a pointer to
   the next node in the list. The LinkedList then keep track of the head and usually the tail (I would suggest
   keeping track of the tail because it makes pop really easy.) As you may have notice, the unit tests are the
   same as the ArrayList; the interface of the two are exactly the same and should make no difference to the
   consumer of the data structure.
-  
+
   length - integer  - How many elements in the list
   push   - function - accepts a value and adds to the end of the list
   pop    - function - removes the last value in the list and returns it
   get    - function - accepts an index and returns the value at that position
-  delete - function - accepts an index, removes value from list, collapses, 
+  delete - function - accepts an index, removes value from list, collapses,
                       and returns removed value
-                      
+
   I would suggest making a second class, a Node class. However that's up to you how you implement it. A Node
   has two properties, value and next.
 
@@ -32,7 +32,7 @@ class LinkedList {
   push(value) {
     const node = new Node(value);
     this.length++;
-    if (!this.head) {
+    if(!this.head) {
       this.head = node;
     } else {
       this.tail.next = node;
@@ -44,12 +44,11 @@ class LinkedList {
   }
   _find(index) {
     if (index >= this.length) return null;
-    let current = this.head;
+    let node = this.head;
     for (let i = 0; i < index; i++) {
-      current = current.next;
+      node = node.next;
     }
-
-    return current;
+    return node;
   }
   get(index) {
     const node = this._find(index);
@@ -65,17 +64,19 @@ class LinkedList {
         this.head = null;
         this.tail = null;
       }
-      this.length--;
+      this.length --;
       return head.value;
     }
 
     const node = this._find(index - 1);
-    const excise = node.next;
-    if (!excise) return null;
-    node.next = excise.next;
-    if (!node.next) this.tail = node.next;
+    const nodeToExcise = node.next;
+
+    if (!nodeToExcise) return null;
+    node.next = nodeToExcise.next;
+    if(!node.next) this.tail = node.next; //wouldn't this be node
+
     this.length--;
-    return excise.value;
+    return nodeToExcise.value;
   }
 }
 
