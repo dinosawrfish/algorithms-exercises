457e1b533a5c29d844a4bfb0214a19cd9cf1c5da
dinosawrfish <dinosawrfish@gmail.com>
2024-09-25 01:13:52 -0700

    bst graph solution

--- specs/graph/graph.test.js
+++ specs/graph/graph.test.js
@@ -6,7 +6,7 @@
 /*
   parameters:
   myId                - number    - the id of the user who is the root node
-  
+
   degreesOfSeparation - number   - how many degrees of separation away to look on the graph
 */
 
@@ -23,16 +23,59 @@
     connections: [687, 997, 437]
   }
 */
-const { getUser } = require("./jobs");
+const { getUser } = require("./jobs"); //getUser(124);
 
 const findMostCommonTitle = (myId, degreesOfSeparation) => {
   // code goes here
+  let queue = [myId];
+  const seen = new Set(queue);
+  const jobs = {};
+
+  for (let i = 0; i <= degreesOfSeparation; i++) {
+    const newQueue = [];
+    while (queue.length) {
+      const user = getUser(queue.shift());
+
+      for (let j = 0; j < user.connections.length; j++) {
+        const connection = user.connections[j];
+        if (!seen.has(connection)) {
+          newQueue.push(connection);
+          seen.add(connection);
+        }
+      }
+
+      jobs[user.title] = jobs[user.title] ? jobs[user.title] + 1 : 1;
+    }
+
+    queue = newQueue;
+  }
+
+  const jobKeys = Object.keys(jobs);
+  let biggestNumber = jobs[jobKeys[0]];
+  let jobName = jobKeys[0];
+
+  for (let i = 1; i < jobKeys.length; i++) {
+    const currentJob = jobKeys[i];
+    if (jobs[currentJob] > biggestNumber) {
+      jobName = currentJob;
+      biggestNumber = jobs[currentJob];
+    }
+  }
+
+  return jobName;
 };
+
+// jobKeys
+//   .map((id) => [id, jobs[id]])
+//   .sort((a, b) => b[1] - a[1])
+//   .slice(0, 10)
+//   .forEach(([job, num]) => console.log(`${job}`));
+// console.log("========")
+
 
 // unit tests
 // do not modify the below code
-test.skip("findMostCommonTitle", function () {
-  // the getUser function and data comes from this CodePen: https://codepen.io/btholt/pen/NXJGwa?editors=0010
+describe("findMostCommonTitle", function () {
   test("user 30 with 2 degrees of separation", () => {
     expect(findMostCommonTitle(30, 2)).toBe("Librarian");
   });
@@ -48,7 +91,7 @@ test.skip("findMostCommonTitle", function () {
   });
 });
 
-test.skip("extra credit", function () {
+describe("extra credit", function () {
   test("user 1 with 7 degrees of separation – this will traverse every user that's followed by someone else. five users are unfollowed", () => {
     expect(findMostCommonTitle(1, 7)).toBe("Geological Engineer");
   });
